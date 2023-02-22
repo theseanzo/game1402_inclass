1112788b7b3bd6470a2ec0981d269d452bc627b2
Sean Lynch <seanzolynch@gmail.com>
2023-02-22 13:34:04 -0700

    Added files for the in class assignment
    Download this.

---
+++ Assets/Scripts/UI/MainMenuScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 1;
+    }
+    public void OnStartGameButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
