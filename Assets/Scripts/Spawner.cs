74226fc283b88e7283465eab2a4d548bf5888cd0
Sean Lynch <seanzolynch@gmail.com>
2023-02-22 14:03:43 -0700

    Changes
    Added changes for the in class assignment

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public AIController aiPrefab;
+    public float spawnRange = 3;
+    public float interval;
+    private float timer = Mathf.Infinity;
+
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+
+    }
+}
